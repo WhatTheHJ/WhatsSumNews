aa958b38fc87f0c4dd3865f290ff0c77ee79a730
WhatTheHJ <fhseh14@gmail.com>
2025-03-11 23:43:34 +0900

    5

--- src/main/java/com/WhatsSumNews/config/WebConfig.java
+++ src/main/java/com/WhatsSumNews/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.WhatsSumNews.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}").setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css|\\.ico|\\.json)$}")
+                .setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
